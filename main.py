c85de267e95ed598492951d63fa6f41273f4a0a4
OrneryCrow <ornery_crow@hotmail.com>
2018-10-04 12:31:20 -0500

    Finally finished creating the basic framework of the user-signup page

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-from flask import Flask, request, render_template
+from flask import Flask, redirect, request, render_template
+import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/', methods=['GET'])
-def home():
-    return render_template('sign-up.html')
 
-@app.route('/greeting', methods=['POST'])
-def greeting():
-    return render_template('greeting.html')
+@app.route("/", methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        user = request.form['username']
+        # Include validation for username
 
+        return render_template('greeting.html', name=user)
 
-
-
-
+    return render_template('index.html')
 
 
 
